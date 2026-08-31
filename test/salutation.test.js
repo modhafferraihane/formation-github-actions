7148b32f5f524d6a70a0416219c7c090f22ad42c
modhafferraihane <modhaffer.raihane@hotmail.com>
2026-08-31 07:47:18 +0100

    fix: correct greeting message in saluer test

--- test/salutation.test.js
+++ test/salutation.test.js
@@ -3,7 +3,7 @@ const assert = require("node:assert");
 const { saluer, aujourdhui } = require("../src/salutation");
 
 test("saluer ajoute le nom", () => {
-  assert.strictEqual(saluer("Ada"), "Bonjour Ada");
+  assert.strictEqual(saluer("Ada"), "Bonjours Ada");
 });
 
 test("saluer refuse un nom vide", () => {
